7ad93642f5d56122b323a5073e5e2f2abb89f37f
Chekesh <122931647+Chekesh@users.noreply.github.com>
2023-02-25 10:27:02 +0300

    Cleaned

--- Kliker/app/src/main/java/com/example/kliker/MainActivity.java
+++ Kliker/app/src/main/java/com/example/kliker/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "StartActivity";
     private Integer counter=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onCreate");
-
     }
 
     @Override
@@ -31,57 +28,23 @@ public class MainActivity extends AppCompatActivity {
                 savedInstanceState.containsKey("counter")){
             counter = savedInstanceState.getInt("counter");
         }
-        Log.d(TAG, "onRestoreInstanceState");
     }
 
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-    }
 
     @Override
     protected void onStart(){
         super.onStart();
-        Log.d(TAG, "onStart");
         resetUI();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
         outState.putInt("counter", counter);
-        Log.d(TAG, "onSaveInstanceState");
     }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
 
     private void resetUI() {
         ((TextView)findViewById(R.id.txt_counter)).setText(counter.toString());
-        Log.d(TAG, "resetUI");
     }
 
 
